708e7e3e05b0a5ad5189d982b14b2935c986f162
Mouad Agzennai <157255208+mouad177@users.noreply.github.com>
2024-08-30 20:35:24 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Products from './pages/Products'
@@ -46,7 +46,7 @@ function App() {
 
   return (
     <>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter >
     <Routes>
       <Route index element={<Home/>}/>
       <Route path='/Home' element={<Home/>}/>
@@ -93,7 +93,7 @@ function App() {
 
 
     </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </>
   )
 }
